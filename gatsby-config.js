197114a0538e808c98f50a35b00ade12b4ec01ec
Dennis Brown <vilesyn@gmail.com>
2021-08-12 17:26:56 -0500

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,43 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-theme-material-ui',
+      options: {
+        webFontsConfig: {
+          // fonts: {
+          //   google: [
+          //     {
+          //       family: `Montserrat`,
+          //       variants: [`300`, `400`, `500`],
+          //     },
+          //   ],
+          // },
+        }
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-react-redux',
+      options: {
+        // [required] - path to your createStore module
+        pathToCreateStoreModule: './src/state/createStore',
+        // [optional] - options passed to `serialize-javascript`
+        // info: https://github.com/yahoo/serialize-javascript#options
+        // will be merged with these defaults:
+        serialize: {
+          space: 0,
+          // if `isJSON` is set to `false`, `eval` is used to deserialize redux state,
+          // otherwise `JSON.parse` is used
+          isJSON: true,
+          unsafe: false,
+          ignoreFunction: true
+        },
+        // [optional] - if true will clean up after itself on the client, default:
+        cleanupOnClient: true,
+        // [optional] - name of key on `window` where serialized state will be stored, default:
+        windowKey: '__PRELOADED_STATE__'
+      }
+    },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
