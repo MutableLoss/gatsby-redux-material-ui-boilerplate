69bf57fe22d83c9b48c002bd41e33cb286adf08d
Dennis Brown <vilesyn@gmail.com>
2021-08-21 17:21:40 -0500

    update jest-preprocess for TS

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -1,5 +1,7 @@
 const babelOptions = {
-  presets: ["babel-preset-gatsby"],
-  // presets: ["babel-preset-gatsby", "@babel/preset-typescript"],
+  presets: [
+    "babel-preset-gatsby",
+    // "@babel/preset-typescript",
+  ],
 }
 module.exports = require("babel-jest").default.createTransformer(babelOptions)
