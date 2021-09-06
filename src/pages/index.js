2f94d1bff325bfc75bd747aa7f155eeb3f60882b
Dennis Brown <vilesyn@gmail.com>
2021-09-06 17:19:22 -0500

    update default template for repo

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site with <a href="https://redux.js.org/">Redux</a> and <a href="https://material-ui.com/" target="_blank">Material-UI</a>.</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
