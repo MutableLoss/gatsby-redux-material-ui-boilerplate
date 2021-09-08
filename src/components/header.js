853975242d02af43aa3efdea10064d1a40064795
Dennis Brown <vilesyn@gmail.com>
2021-09-07 19:23:56 -0500

    update header with MUI theme styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,46 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { makeStyles } from "@material-ui/core/styles"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
+const Header = ({ siteTitle }) => {
+  const useStyles = makeStyles(theme => ({
+    header: {
+      background: theme.palette.primary.main,
       marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+    },
+    link: {
+      color: theme.palette.primary.contrastText,
+      textDecoration: `none`,
+    },
+    title: {
+      margin: `0 auto`,
+      maxWidth: 960,
+      padding: `1.45rem 1.0875rem`,
+    }
+  }))
+
+  const classes = useStyles()
+
+  return (
+    <header
+      className={classes.header}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <div
+        className={classes.title}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            className={classes.link}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
