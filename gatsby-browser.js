1df62de370d2d3028fff51640cf4b995662d7ab3
Dennis Brown <vilesyn@gmail.com>
2021-09-11 15:37:48 -0500

    remove top-layout from browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,21 +7,14 @@
 // You can delete this file if you're not using it
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createTheme, responsiveFontSizes } from '@material-ui/core/styles';
-import AppTheme from './static/theme';
-import ThemeTopLayout from 'gatsby-theme-material-ui-top-layout/src/components/top-layout';
 import createStore from './src/state/createStore'
 
 export const wrapRootElement = ({ element, props }) => {
   const store = createStore()
-  let theme = createTheme(AppTheme)
-  theme = responsiveFontSizes(theme)
 
   return (
     <Provider store={store}>
-      <ThemeTopLayout theme={theme}>
-        {element}
-      </ThemeTopLayout>
+      {element}
     </Provider>
   )
 }
