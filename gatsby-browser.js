c7ec964d60bc660a5966677fc0a0c6cd516889b9
Dennis Brown <vilesyn@gmail.com>
2021-09-17 16:08:21 -0500

    move top-layout to the browser config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,14 +7,39 @@
 // You can delete this file if you're not using it
 import React from 'react'
 import { Provider } from 'react-redux'
+import { Helmet } from 'react-helmet';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import createStore from './src/state/createStore'
+
+function TopLayout(props) {
+  return (
+    <React.Fragment>
+      <Helmet>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        {props.children}
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
 
 export const wrapRootElement = ({ element, props }) => {
   const store = createStore()
 
   return (
     <Provider store={store}>
-      {element}
+      <TopLayout>
+        {element}
+      </TopLayout>
     </Provider>
   )
 }
